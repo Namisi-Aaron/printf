becf8be7a4b4aacacd8e352fab491bca75bb151e
Janet Shitandi <janetshitandi99@gmail.com>
2023-08-22 03:23:46 -0700

    changes

---
+++ test_fun/_printf.c
@@ -0,0 +1,64 @@
+#include "main.h"
+/**
+ * _printf - produces output according to a format
+ * @format: string input
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int print_chara;
+	va_list myList_args;
+	int length_string;
+	char ch;
+	char *myString;
+
+	print_chara = 0;
+	length_string = 0;
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(myList_args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			print_chara++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				print_chara++;
+			}
+			else if (*format == 's')
+			{
+				myString = va_arg(myList_args, char*);
+				while (myString[length_string] != '\0')
+				{
+					length_string++;
+				}
+				write(1, myString, length_string);
+				print_chara += length_string;
+			}
+			else if (*format == 'c')
+			{
+				ch = va_arg(myList_args, int);
+				write(1, &ch, 1);
+				print_chara++;
+			}
+		}
+		format++;
+	}
+	va_end(myList_args);
+	return (print_chara);
+}
