5622a551ea9420acb1cd40524229a1a2ffabcf18
Namisi-Aaron <aaronnamisi@gmail.com>
2023-08-22 13:24:32 +0300

    task 1 files

---
+++ test_fun/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests code
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+	    _printf("Character:[%c]\n", 'H');
+	    _printf("String:[%s]\n", "I am a string !");
+	    _printf("Percent:[%%]\n");
+	    return (1);
+}
